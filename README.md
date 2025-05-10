88c2a9086cb752cf2e7ab7a8bf50984b34c0476b
Vitor Ramos <vitorramosnascimento10@gmail.com>
2025-05-10 12:02:33 -0300

    finalmentes, desculpa a demora

---
+++ README.md
@@ -0,0 +1,35 @@
+# 🌳 Árvores Binárias
+
+📘 **Trabalho 2 — Técnica de Programação Avançada**  
+Tema: Implementação e manipulação de **Árvores Binárias**
+
+---
+
+## 📂 Descrição
+
+Este repositório contém a implementação do segundo trabalho da disciplina **Técnica de Programação Avançada**, cujo foco é a criação e manipulação de **estruturas de Árvores Binárias**.
+
+---
+
+## 🧠 Conteúdos abordados
+- Adicionar
+
+---
+
+## 🛠️ Tecnologias utilizadas
+
+- 💻 Linguagem: Java  
+- 🎨 Interface Gráfica: JavaFX  
+
+---
+
+## 📎 Organização do projeto
+- Adicionar
+
+---
+
+## 🚀 Como executar
+- Adicionar
+
+## 👨‍🏫 Autoria
+- Adicionar
---
+++ src/appLibrary/application/TesteArvoreLivro.java
@@ -0,0 +1,44 @@
+package appLibrary.application;
+
+import appLibrary.model.entities.Livro;
+import appLibrary.model.comparators.ComparadorID;
+import lib.ArvoreBinaria;
+
+public class TesteArvoreLivro {
+    public static void main(String[] args) {
+        ArvoreBinaria<Livro> arvore = new ArvoreBinaria<>(new ComparadorID());
+
+        Livro l1 = new Livro(10, "1984", "George Orwell", "Ficção");
+        Livro l2 = new Livro(5, "Dom Casmurro", "Machado de Assis", "Romance");
+        Livro l3 = new Livro(15, "O Hobbit", "Tolkien", "Fantasia");
+        Livro l4 = new Livro(3, "A Revolução dos Bichos", "Orwell", "Ficção");
+        Livro l5 = new Livro(7, "Memórias Póstumas", "Machado", "Romance");
+
+        // Inserções
+        arvore.adicionar(l1);
+        arvore.adicionar(l2);
+        arvore.adicionar(l3);
+        arvore.adicionar(l4);
+        arvore.adicionar(l5);
+
+
+        // Imprimir
+        System.out.println("Imprimindo em ordem: " + arvore.caminharEmOrdem());
+        System.out.println("Imprimindo em nivel: " + arvore.caminharEmNivel());
+        System.out.println("Imprimindo em pós-ordem: " + arvore.caminharPosOrdem());
+        System.out.println("Altura: " + arvore.altura());
+
+
+        // Pesquisas
+        System.out.println("\nPesquisa Livro com ID 7: " + arvore.pesquisar(l5));  // deve encontrar
+        System.out.println("Pesquisa Livro com ID 20 (inexistente): " + arvore.pesquisar(new Livro(20, "", "", "")));  // deve retornar null
+
+        // Remoção
+        System.out.println("\nRemoção do livro com ID 3: " + arvore.remover(l4));  // nó folha
+        System.out.println("Remoção do livro com ID 5: " + arvore.remover(l2));  // nó com 1 filho
+        System.out.println("Remoção do livro com ID 10: " + arvore.remover(l1)); // nó com 2 filhos
+
+
+        System.out.println("\nVerifica se livro ID 10 ainda existe: " + arvore.pesquisar(l1));
+    }
+}
